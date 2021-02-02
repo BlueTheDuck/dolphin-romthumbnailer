6224f2901131809534499c83d7c54b9ef35fe9d9
BlueTheDuck <pedroignacioperezvargas@gmail.com>
2021-02-02 01:05:13 -0300

    Improve tools

--- build.sh
+++ build.sh
@@ -3,8 +3,7 @@
 cd build \
   && cmake -DKDE_INSTALL_USE_QT_SYS_PATHS=ON -DCMAKE_INSTALL_PREFIX=`kf5-config --prefix` .. \
   && make \
-  && cd tests && ctest && cd .. \
+  && cd tests && ctest --output-on-failure && cd .. \
   && [ "$1" != "dry" ] \
-  && sudo make uninstall \
-  && sudo make install \
+  && (sudo make uninstall; sudo make install) \
   && kbuildsycoca5
